575218e7af8467d51b5e4e57935b176846af8b5a
cassidyb <cassidybollin32@gmail.com>
2016-11-15 11:59:02 -0800

    added number game

--- survey_counter/server.py
+++ survey_counter/server.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, request, redirect, session
+import random
 app = Flask(__name__)
 app.secret_key = 'TopSecretMate'
 
 @app.route('/')
 def index():
+    # counter for website counter
     counter = 1
     session['counter'] += counter
+    session['someKey'] = 50
+    # random number for guessing game
+    number = random.randrange(0,101)
+    session['number'] = number
+    session['guessstatus'] = ''
+    print number
     return render_template('index.html')
 
 @app.route('/double')
@@ -25,12 +33,25 @@ def show_user():
 
 @app.route('/users', methods=['POST'])
 def create_user():
-    print "Got Post Information"
     session['name'] = request.form['name']
     session['location'] = request.form['location']
     session['language'] = request.form['language']
     session['comment'] = request.form['comment']
-    # print name, location, language, comment
     return redirect('/show')
 
+# guessing game route
+@app.route('/takeguess', methods=['POST'])
+def take_guess():
+    guess = int(request.form['guess'])
+    number = session['number']
+    if guess == number:
+        print 'correct'
+        session['guessstatus'] = 'correct'
+    elif guess < number:
+        print 'too low'
+        session['guessstatus'] = 'too low'
+    elif guess > number:
+        print 'too high'
+        session['guessstatus'] = 'too high'
+    return render_template('/index.html')
 app.run(debug=True)
